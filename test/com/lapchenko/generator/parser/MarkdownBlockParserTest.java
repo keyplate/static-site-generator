701aad79d19caaa1d4b6ccaf0560f8ead92a7d13
keyplate <nevak36@gmail.com>
2024-08-07 14:27:56 +0200

    Add tests for md block parser

--- test/com/lapchenko/generator/parser/MarkdownBlockParserTest.java
+++ test/com/lapchenko/generator/parser/MarkdownBlockParserTest.java
@@ -4,12 +4,14 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 import com.lapchenko.generator.parser.MarkdownBlockParser;
 import com.lapchenko.generator.parser.BlockFormat;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class MarkdownBlockParserTest {
     
     private final static MarkdownBlockParser parser = new MarkdownBlockParser(); 
 
+    //GENERAL
     @Test
     void blocksSplittedCorrectly() {
         var blocks = """
@@ -29,13 +31,29 @@ public class MarkdownBlockParserTest {
         assertTrue(expected.containsAll(parser.markdownToBlocks(blocks)));
     }
     
+    //HEADINGS
     @Test
-    void headingBlock() {
+    void headingBlock1() {
         var heading = "# Heading";
         var blocks = parser.blockToBlockTypes(List.of(heading));
-        assertTrue(blocks.get(heading) == BlockFormat.HEADING);
+        assertSame(BlockFormat.HEADING, blocks.get(heading));
+    }
+
+    @Test
+    void headingBlock2() {
+        var heading = "## Heading";
+        var blocks = parser.blockToBlockTypes(List.of(heading));
+        assertSame(BlockFormat.HEADING, blocks.get(heading));
     }
     
+    @Test
+    void headingBlockHashRedundant() {
+        var heading = "####### Heading";
+        var blocks = parser.blockToBlockTypes(List.of(heading));
+        assertSame(BlockFormat.PARAGRAPH, blocks.get(heading));
+    }
+
+    //QUOTES
     @Test
     void quoteBlock() {
         var quote = """
@@ -43,6 +61,47 @@ public class MarkdownBlockParserTest {
         > This is also a quote
         """;
         var blocks = parser.blockToBlockTypes(List.of(quote));
-        assertTrue(blocks.get(quote) == BlockFormat.QUOTE);
+        assertSame(BlockFormat.QUOTE, blocks.get(quote));
+    }
+
+    //CODE
+    @Test
+    void codeBlock() {
+        var code = """
+        ```
+        This is a code block
+        ```
+        """;
+        var blocks = parser.blockToBlockTypes(List.of(code));
+        assertSame(BlockFormat.CODE, blocks.get(code));
+    }
+
+    @Test
+    void codeBlockInline() {
+        var code = "```{ var = This; is++; a code block}```";
+        var blocks = parser.blockToBlockTypes(List.of(code));
+        assertSame(BlockFormat.CODE, blocks.get(code));
+    }
+
+    //UNORDERED LIST
+    void unorderedList() {
+        var list = """
+        * This
+        * is
+        * an item
+        """;
+        var blocks = parser.blockToBlockTypes(List.of(list));
+        assertSame(BlockFormat.UNORDERED_LIST, blocks.get(list));
+    }
+
+    //ORDERED LIST
+    void unorderedList() {
+        var list = """
+        1 first
+        2 second
+        3 third
+        """;
+        var blocks = parser.blockToBlockTypes(List.of(list));
+        assertSame(BlockFormat.ORDERED_LIST, blocks.get(list));
     }
 }
